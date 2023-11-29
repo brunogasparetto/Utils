e73c55506012ff252999126ac2b223f9d1d31b18
Bruno Gasparetto <brunogasparetto@gmail.com>
2023-11-28 20:28:12 -0400

    Troca create_function (removido) por arrow function

--- name_capitalize.php
+++ name_capitalize.php
@@ -6,18 +6,19 @@
  * @param string $string
  * @return string
  */
-private function name_capitalize($string) {
+function name_capitalize($string) {
     $string = mb_convert_case(trim($string), MB_CASE_TITLE);
 
     // para o caso do À que não é reconhecido facilmente \x{00C0}
     $string = preg_replace_callback(
         '/\s(e|\x{00C0}|da|das|de|di|do|dos)[\s,]/iu',
-        create_function('$matches','return mb_strtolower($matches[0]);'),
+        fn($matches) => mb_strtolower($matches[0])
         $string
     );
 
     $string = preg_replace_callback(
-        '/\b([IVXLCDM]+)\b/iu', create_function('$matches','return mb_strtoupper($matches[0]);'),
+        '/\b([IVXLCDM]+)\b/iu',
+        fn($matches) => mb_strtoupper($matches[0])
         $string
     );
 
